fac64c148e15a70d5ee5cd699c714320f6ef4019
stringa <sstringa@gmail.com>
2017-05-04 13:04:39 -0700

    creates stream, but never gets the remote stream

--- www/broadcast/js/sfu.js
+++ www/broadcast/js/sfu.js
@@ -1,36 +1,61 @@
-const url = "wss://"+window.location.hostname+":"+window.location.port;
 
-const roomId = (new Date()).getTime() + "-" + Math.random();
+var url = "wss://"+window.location.hostname+":"+window.location.port;
+var roomId = (new Date()).getTime() + "-" + Math.random();
 
 
-function addVideoForStream(stream,muted)
+function addVideoForStream(stream,isLocal)
 {
-	//Create new video element
-	const video = document.querySelector (muted ? "#local" : "#remote");
-	//Set same id
-	video.id = stream.id;
-	//Set src stream
-	video.src = URL.createObjectURL(stream);
-	//Set other properties
-	video.autoplay = true;
-	video.muted = muted;
+    //Create new video element
+    const video = document.querySelector (isLocal ? "#localVideo" : "#remoteVideo");
+    //Set same id
+    video.id = stream.id;
+    //Set src stream
+    video.src = URL.createObjectURL(stream);
+    //Set other properties
+    video.autoplay = true;
+    video.muted = true;
 }
 function removeVideoForStream(stream)
 {
-	//Get video
-	var video = document.getElementById(stream.id);
-	//Remove it when done
-	video.addEventListener('webkitTransitionEnd',function(){
-            //Delete it
-	    video.parentElement.removeChild(video);
-        });
-	//Disable it first
-	video.className = "disabled";
+    //Get video
+    var video = document.getElementById(stream.id);
+    //Remove it when done
+    video.addEventListener('webkitTransitionEnd',function(){
+        //Delete it
+        video.parentElement.removeChild(video);
+    });
+    //Disable it first
+    video.className = "disabled";
 }
 
 var sdp;
 var pc;
-	
+
+var localStream;
+var remoteStream;
+var localVideo;
+var ws;
+
+function pageReady() {
+
+    localVideo = document.querySelector('#localVideo');
+	var constraints = {
+        audio: false,
+        video: true
+    };
+
+    if(navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        	addVideoForStream(stream, true);
+		}).catch((e) => {
+            console.error(e);
+		});
+    } else {
+        alert('Your browser does not support getUserMedia API');
+    }
+}
+
+
 function connect() 
 {
 
@@ -39,18 +64,14 @@ function connect()
 			bundlePolicy: "max-bundle",
 			rtcpMuxPolicy : "require"
 		});
-	else 
+	else
 		pc = new webkitRTCPeerConnection(null);
-	
-	var ws = new WebSocket(url,"broadcast");
 	
 	pc.onaddstream = function(event) {
 		var prev = 0;
 		console.debug("onAddStream",event);
 		//Play it
-		addVideoForStream(event.stream);
-
-			
+		addVideoForStream(event.stream, false);
 	};
 	
 	pc.onremovestream = function(event) {
@@ -58,34 +79,39 @@ function connect()
 		//Play it
 		removeVideoForStream(event.stream);
 	};
-	
-	ws.onopen = function(){
+
+
+    var url = "wss://"+window.location.hostname+":"+window.location.port;
+    var ws = new WebSocket(url,"broadcast");
+
+    ws.onopen = function(){
 		console.log("opened");
 		
-	//Create new offer
-	pc.createOffer({
-			offerToReceiveVideo: true
-		})
-		.then(function(offer){
-			console.debug("createOffer sucess",offer);
-			//We have sdp
-			sdp = offer.sdp;
-			//Set it
-			pc.setLocalDescription(offer);
-			console.log(sdp);
-			//Create room
-			ws.send(JSON.stringify({
-				cmd		: "OFFER",
-				offer		: sdp
-			}));
-		})
-		.catch(function(error){
-			console.error("Error",error);
-		});
+		//Create new offer
+		pc.createOffer({
+				offerToReceiveVideo: true
+			})
+			.then(function(offer){
+				console.debug("createOffer sucess",offer);
+				//We have sdp
+				sdp = offer.sdp;
+				//Set it
+				pc.setLocalDescription(offer);
+				console.log(sdp);
+				//Create room
+				ws.send(JSON.stringify({
+					cmd		: "OFFER",
+					offer		: sdp
+				}));
+			})
+			.catch(function(error){
+				console.error("Error",error);
+			});
 	};
 	
 	ws.onmessage = function(event){
-		console.log(event);
+		debugger
+		// console.log(event);
 		
 		//Get protocol message
 		const msg = JSON.parse(event.data);
@@ -102,19 +128,6 @@ function connect()
 		);
 	};
 }
-
-var dialog = document.querySelector('dialog');
-if (dialog.showModal)
-{
-	dialog.showModal();
-	dialog.querySelector('.ready').addEventListener('click', function() {
-		dialog.close();
-		connect();
-	});
-} else {
-	connect();
-}
-
 
 
 
